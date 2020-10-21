e76205304b26756bf95a3df47cef87390433ad2d
vikrosj <viktoria.rosjo@gmail.com>
2020-10-21 20:22:52 +0200

    Create load_partial

---
+++ import_weights.py
@@ -0,0 +1,15 @@
+from functools import partial
+from torch import torch
+from pathlib import Path, PurePath
+
+base_url = Path('weights').resolve()
+
+def load_partial(url_path):
+
+    url = PurePath(base_url, url_path)
+
+    partial_load = partial(torch.load, url)
+
+    return load_partial
+
+
