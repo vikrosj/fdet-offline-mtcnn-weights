4b6a1116c237679f7f93184973453da93884b6a3
vikrosj <viktoria.rosjo@gmail.com>
2020-10-21 20:54:42 +0200

    Edit import_weights.py

--- fomw/import_weights.py
+++ fomw/import_weights.py
@@ -2,9 +2,18 @@ from functools import partial
 from torch import torch
 from pathlib import Path, PurePath
 
-base_url = Path('weights').resolve()
+base_url = Path('weights/').resolve()
 
-def load_partial(url_path):
+def load_partial(mtcnn_type):
+
+    if mtcnn_type == 'onet':
+        url_path = 'mtcnn_onet.pt'
+    if mtcnn_type == 'pnet':
+        url_path = 'mtcnn_pnet.pt'
+    if mtcnn_type == 'rnet':
+        url_path = 'mtcnn_rnet.pt'
+    else:
+        print(f'mtcnn_type is not recognized: {mtcnn_type}')
 
     url = PurePath(base_url, url_path)
 
